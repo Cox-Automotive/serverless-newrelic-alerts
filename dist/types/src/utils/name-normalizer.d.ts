a653bb08d06cf0754cfeec6edcaef7074d07dc57
Aleksandr Vorobiev <Aleksandr.Vorobiev-ext@renaissance.com>
2020-12-02 23:27:57 +0300

    fix: Build dist

--- dist/types/src/utils/name-normalizer.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function getNormalizedPolicyName(functionName: string, policyName: string): string;
-export declare const getNormalizedFilterItemName: (functionName: string, filterItemName?: string) => string;
-export declare const getNormalizedInfrastructureConditionName: (functionName: string, conditionName: string) => string;
-export declare const getNormalizedNrqlConditionName: (functionName: string, conditionName: string) => string;
