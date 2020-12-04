7badf62f06dce5466c8919b67d3c4cd910eb64a8
Aleksandr Vorobiev <Aleksandr.Vorobiev-ext@renaissance.com>
2020-12-04 14:34:20 +0300

    fix: test

--- test-integration/app-under-test/sls-package.sh
+++ test-integration/app-under-test/sls-package.sh
@@ -1,7 +1,8 @@
+rm -rf serverless-newrelic-alerts
 mkdir serverless-newrelic-alerts
-cp ../../../package.json serverless-newrelic-alerts/
-cp ../../../package-lock.json serverless-newrelic-alerts/
-cp -R ../../../dist serverless-newrelic-alerts/
+cp ../../package.json serverless-newrelic-alerts/
+cp ../../package-lock.json serverless-newrelic-alerts/
+cp -R ../../dist serverless-newrelic-alerts/
 
 npm install
 
