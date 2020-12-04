8532f7f99ac8b96e0dfcfe47a1892abae8e54609
Aleksandr Vorobiev <Aleksandr.Vorobiev-ext@renaissance.com>
2020-12-04 14:26:42 +0300

    fix: test

--- test-integration/sls-package.test.ts
+++ test-integration/sls-package.test.ts
@@ -3,11 +3,12 @@ import {execSync} from 'child_process';
 describe('Serverless package', () => {
   it('Should generate CloudFormation code', async () => {
     try {
-      const stdout = execSync('./sls-package.sh', {cwd: './test/system/app-under-test'});
+      const stdout = execSync('./sls-package.sh', {cwd: './test-integration/app-under-test'});
       console.log(stdout.toString())
     } catch (error) {
       console.log('stdout: ', error.stdout?.toString());
       console.log('stderr: ', error.stderr?.toString());
+      throw error
     }
     const cfTemplate = require('./app-under-test/.serverless/cloudformation-template-update-stack.json');
     const expectedCfTemplate = require('./app-under-test/extected-cloudformation-template-update-stack.json');
